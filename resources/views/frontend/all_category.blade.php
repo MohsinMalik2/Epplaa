d5b34d3523f04d484f2a8bfbb09c4185b12de6e0
MohsinMalik01 <malikmuhsin70@gmail.com>
2021-12-28 01:33:38 +0500

    New pages epplla

--- resources/views/frontend/all_category.blade.php
+++ resources/views/frontend/all_category.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-<section class="pt-4 mb-4">
+<!-- <section class="pt-4 mb-4">
     <div class="container text-center">
         <div class="row">
             <div class="col-lg-6 text-center text-lg-left">
@@ -46,6 +46,77 @@
             </div>
         @endforeach
     </div>
-</section>
+</section> -->
+<main class="main">
+    <!-- Start of Page Header -->
+    <div class="page-header">
+        <h1 class="page-title">{{ translate('All Categories') }}</h1>
+        <h4 class="page-subtitle"> & Sub Categories .</h4>
+    </div>
+    <!-- End of Page Header -->
 
+    <!-- Start of Breadcrumb -->
+    <nav class="breadcrumb-nav mb-10 pb-1">
+        <div class="container">
+            <ul class="breadcrumb">
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <!-- <li><a href="{{ route('categories.all') }}">{{ translate('All Categories') }}</a></li> -->
+                <li>{{ translate('All Categories') }}</li>
+            </ul>
+        </div>
+    </nav>
+    <!-- End of Breadcrumb -->
+
+            <div class="page-content pb-10 mb-2">
+                <div class="container">
+                    <section class="category-section mb-10 pb-1">
+                    @foreach ($categories as $key => $category)
+                        <h2 class="title title-center mb-5"><a href="{{ route('products.category', $category->slug) }}">{{  $category->getTranslation('name') }}</a></h2>
+                        <div class="swiper-container swiper-theme show-code-action" data-swiper-options="{
+                            'spaceBetween': 20,
+                            'slidesPerView': 1,
+                            'breakpoints': {
+                                '576': {
+                                    'slidesPerView': 2
+                                },
+                                '992': {
+                                    'slidesPerView': 3
+                                }
+                            }
+                            }">
+                            <div class="swiper-wrapper row cols-lg-3 cols-md-2 cols-1">
+                                @foreach (\App\Utility\CategoryUtility::get_immediate_children_ids($category->id) as $key => $first_level_id)
+                                    <div class="swiper-slide category-wrap">
+                                        <div class="category category-group-image">
+                                            <div class="category-content">
+                                                
+                                                <h4 class="category-name"><a href="{{ route('products.category', \App\Models\Category::find($first_level_id)->slug) }}">{{ \App\Models\Category::find($first_level_id)->getTranslation('name') }}</a>
+                                                </h4>
+                                                <ul class="category-list">
+                                                    @foreach (\App\Utility\CategoryUtility::get_immediate_children_ids($first_level_id) as $key => $second_level_id)
+                                                        <li><a href="{{ route('products.category', \App\Models\Category::find($second_level_id)->slug) }}">{{ \App\Models\Category::find($second_level_id)->getTranslation('name') }}</a></li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                            <a href="#">
+                                                <figure class="category-media">
+                                                    <img src="assets/images/categories/category-1.jpg" alt="Category"
+                                                        width="190" height="214" />
+                                                </figure>
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    @endforeach
+                    </section>
+                    <!-- End of Category Section -->
+                </div>
+            </div>
+
+
+                  
+</main>
 @endsection
--- resources/views/frontend/flash_deal/all_flash_deal_list.blade.php
+++ resources/views/frontend/flash_deal/all_flash_deal_list.blade.php
@@ -46,4 +46,70 @@
         </div>
     </div>
 </section>
+
+<main class="main">
+            <!-- Start of Page Header -->
+            <div class="page-header">
+                <h1 class="page-title">{{ translate('Flash Deals')}}</h1>
+            </div>
+            <!-- End of Page Header -->
+
+            <!-- Start of Breadcrumb -->
+            <nav class="breadcrumb-nav mb-10 pb-1">
+                <div class="container">
+                    <ul class="breadcrumb">
+                        <li><a href="demo1.html">Home</a></li>
+                        <li>Flash Deals</li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- End of Breadcrumb -->
+
+            <!-- Start of Page Content -->
+            <div class="page-content pb-10 mb-2">
+                <div class="container">
+                    <section class="category-section category-2cols-simple mb-10 pb-1">
+                        <!-- <h2 class="title title-center mb-5">Flash Deals</h2> -->
+                        <div class="swiper-container swiper-theme show-code-action" data-swiper-options="{
+                            'spaceBetween': 20,
+                            'slidesPerView': 1,
+                            'breakpoints': {
+                                '576': {
+                                    'slidesPerView': 2
+                                },
+                                '992': {
+                                    'slidesPerView': 3
+                                }
+                            }
+                        }">
+                            <div class="swiper-wrapper row cols-lg-3 cols-sm-2 cols-1">
+                                @foreach($all_flash_deals as $single)
+                                    <div class="swiper-slide category-wrap">
+                                        <div class="category category-absolute category-default overlay-zoom">
+                                            <a href="{{ route('flash-deal-details', $single->slug) }}">
+                                                <figure>
+                                                    <img src="{{ uploaded_asset($single->banner) }}"
+                                                        alt="Category Banner" width="400" height="200"
+                                                        style="background-color: #423D39;" />
+                                                </figure>
+                                            </a>
+                                            <div class="category-content y-50">
+                                                <h4 class="category-title text-white font-weight-bolder">{{ $single->title }}</h4>
+                                                
+                                                <a href="{{ route('flash-deal-details', $single->slug) }}"
+                                                    class="btn btn-white btn-link btn-underline">Shop Now</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </section>
+                    <!-- End of Cateogry Section .category-2cols-simple -->
+
+                </div>
+            </div>
+</main>
+
 @endsection
